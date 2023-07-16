cdb2742dc130ef22dcbb4a59e460515ccaaf82fe
AleMot94 <Alector666@gmai.com>
2023-07-16 13:20:08 -0300

    llamando api rick and morty con axios

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import { HeaderComponent } from "../../components";
+import { characters } from "../../api/characters";
 
 export const HomePage: React.FC<{}> = () => {
+
+    React.useEffect(()=>{
+        characters.getAll({page:2}).then((res)=>{
+            console.log(res.data.results)
+        }).catch((e)=>{
+            console.log(e)
+        }),
+
+        characters.getById({id:27}).then((res)=>{
+            console.log(res.data)
+        }).catch((e)=>{
+            console.log(e)
+        })
+    }, [])
 
     return (
         <Container maxWidth="xl">
